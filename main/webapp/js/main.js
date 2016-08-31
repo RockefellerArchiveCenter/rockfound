bc29815f7daa41f39659f730349c5a54aa48874d
Amitkumar Kashyap <Amitkumar.Kashyap1@marist.edu>
2016-08-31 09:42:10 -0400

    new changes for css and portalnormal

--- main/webapp/js/main.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-$(document).ready(function() {
-  $('#evolution-of-a-foundation li').click(function() {
-    var link = $(this).find('a').attr('href');
-    console.log('link', link);
-    return false;
-  });
-});
